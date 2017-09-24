54e3ae2aa2c86e277de4a030b18fa8452f68eaba
waxelfish <axwagner@yahoo.com>
2017-09-24 13:19:03 -0500

    Formated Document

--- Get-NetworkAdapterStatus.ps1
+++ Get-NetworkAdapterStatus.ps1
@@ -25,31 +25,29 @@ Http://www.ScriptingGuys.com
 #Requires -Version 2.0
 #>
 Param(
-    [string]$computer=$env:COMPUTERNAME
-    ) #end param
-    function Get-StatusFromValue
-    {
+    [string]$computer = $env:COMPUTERNAME
+) #end param
+function Get-StatusFromValue {
     Param($SV)
-    switch($SV)
-    {
-    0 { " Disconnected" }
-    1 { " Connecting" }
-    2 { " Connected" }
-    3 { " Disconnecting" }
-    4 { " Hardware not present" }
-    5 { " Hardware disabled" }
-    6 { " Hardware malfunction" }
-    7 { " Media disconnected" }
-    8 { " Authenticating" }
-    9 { " Authentication succeeded" }
-    10 { " Authentication failed" }
-    11 { " Invalid Address" }
-    12 { " Credentials Required" }
-    Default { "Not connected" }
+    switch ($SV) {
+        0 { " Disconnected" }
+        1 { " Connecting" }
+        2 { " Connected" }
+        3 { " Disconnecting" }
+        4 { " Hardware not present" }
+        5 { " Hardware disabled" }
+        6 { " Hardware malfunction" }
+        7 { " Media disconnected" }
+        8 { " Authenticating" }
+        9 { " Authentication succeeded" }
+        10 { " Authentication failed" }
+        11 { " Invalid Address" }
+        12 { " Credentials Required" }
+        Default { "Not connected" }
     }
-    } #end Get-StatusFromValue function
-    # \*\*\* Entry point to script \*\*\*
-    Get-WmiObject -Class win32_networkadapter -computer $computer |
+} #end Get-StatusFromValue function
+# \*\*\* Entry point to script \*\*\*
+Get-WmiObject -Class win32_networkadapter -computer $computer |
     Select-Object Name, @{LABEL = "Status"; EXPRESSION = { Get-StatusFromValue $_.NetConnectionStatus}}
     
     
